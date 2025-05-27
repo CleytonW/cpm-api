746f429479b9121333f34989375aca8d59b756bc
Cleyton Melo <cleytonw11@gmail.com>
2025-05-27 12:30:47 -0300

    Adicionando anotação @Repository a CategoriaRepository

--- src/main/java/br/com/cpm/api/repository/CategoriaRepository.java
+++ src/main/java/br/com/cpm/api/repository/CategoriaRepository.java
@@ -1,9 +1,11 @@
 package br.com.cpm.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.cpm.api.model.Categoria;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
     
 }
