38ba27b7bbed7fae09c97779b83d93ba4d7c3ed6
Cleyton Melo <cleytonw11@gmail.com>
2025-05-27 19:37:06 -0300

    Adicionando funcionalidade para adicionar produto pela rota

--- src/main/java/br/com/cpm/api/controller/ProdutoController.java
+++ src/main/java/br/com/cpm/api/controller/ProdutoController.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,6 +49,11 @@ public class ProdutoController {
         produtoService.deletarProduto(id);
 
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> cadastrarProduto(@RequestBody Produto produto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(produtoService.salvarProduto(produto));
     }
 
     @Autowired
