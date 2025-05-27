b3a7a4dfa2a280680387b762d935c3c5ab89434d
Cleyton Melo <cleytonw11@gmail.com>
2025-05-27 18:18:45 -0300

    Adicionando funcionalidade de listar produtos e carregar pelo produto pelo id

--- src/main/java/br/com/cpm/api/controller/ProdutoController.java
+++ src/main/java/br/com/cpm/api/controller/ProdutoController.java
@@ -1,10 +1,38 @@
 package br.com.cpm.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cpm.api.model.Produto;
+import br.com.cpm.api.service.ProdutoService;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
-    
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> listarProdutos() {
+        return ResponseEntity.ok().body(produtoService.carregarProdutos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Produto>> buscarProdutoPeloId(@PathVariable("id") Long id) {
+        Optional<Produto> produto = produtoService.carregarDadosProdutosPeloId(id);
+
+        if (produto.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok().body(produto);
+    }
+
+    @Autowired
+    private ProdutoService produtoService;
 }
