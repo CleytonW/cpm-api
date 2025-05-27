d7009dc06702060c0e122677d9236573fa06094d
Cleyton Melo <cleytonw11@gmail.com>
2025-05-26 21:20:44 -0300

    Iniciando projeto de cadastro

---
+++ src/main/java/br/com/cpm/api/CpmApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.cpm.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpmApiApplication.class, args);
+	}
+
+}
