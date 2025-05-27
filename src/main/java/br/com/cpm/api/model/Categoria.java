5809c6a73cf67aa08ca812bd638e30ed801a92a8
Cleyton Melo <cleytonw11@gmail.com>
2025-05-27 12:13:29 -0300

    Adicionando entidade produto e categoria ao projeto e fazendo relação entre elas

---
+++ src/main/java/br/com/cpm/api/model/Categoria.java
@@ -0,0 +1,27 @@
+package br.com.cpm.api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "TB_CATEGORIA")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+}
---
+++ src/main/java/br/com/cpm/api/model/Produto.java
@@ -0,0 +1,41 @@
+package br.com.cpm.api.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "TB_PRODUTOS")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+
+    @Column(nullable = false)
+    private BigDecimal preco;
+
+    private Integer quantidadeEstoque;
+
+    @OneToOne
+    @JoinColumn(nullable = false)
+    private Categoria categoria;
+
+}
