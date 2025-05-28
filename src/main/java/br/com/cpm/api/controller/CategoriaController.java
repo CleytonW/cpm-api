540735909bc8269b1a61a9a0352ce473a61f27cf
Cleyton Melo <cleytonw11@gmail.com>
2025-05-28 12:25:57 -0300

    Adicionando funcionalidade de salvar categorias

--- src/main/java/br/com/cpm/api/controller/CategoriaController.java
+++ src/main/java/br/com/cpm/api/controller/CategoriaController.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -48,7 +50,10 @@ public class CategoriaController {
         return ResponseEntity.noContent().build();
     }
 
-
+    @PostMapping
+    public ResponseEntity<Categoria> cadastrarCategoria(@RequestBody Categoria categoria) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoriaService.salvarCategoria(categoria));
+    }
     
 
 
