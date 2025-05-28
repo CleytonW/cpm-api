43cae121b3f050599a602558d5f04cb7a47f2a94
Cleyton Melo <cleytonw11@gmail.com>
2025-05-28 11:46:42 -0300

    Adicionando método para atualizar dados dos produtos

--- src/main/java/br/com/cpm/api/controller/ProdutoController.java
+++ src/main/java/br/com/cpm/api/controller/ProdutoController.java
@@ -5,12 +5,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,6 +54,19 @@ public class ProdutoController {
     @PostMapping
     public ResponseEntity<Produto> cadastrarProduto(@RequestBody Produto produto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(produtoService.salvarProduto(produto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Produto> atualizarProduto(@PathVariable("id") Long id, @RequestBody Produto dadosProduto) {
+        Optional<Produto> produto = produtoService.carregarDadosProdutosPeloId(id);
+
+        if (produto.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        dadosProduto.setId(id);
+
+        return ResponseEntity.ok().body(produtoService.salvarProduto(dadosProduto));
     }
 
     @Autowired
