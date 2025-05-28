260d3b2754921e16f37f77bd8e6df84d2786e681
Cleyton Melo <cleytonw11@gmail.com>
2025-05-28 12:36:44 -0300

    Finalizando controller de categorias com a funcionalide de atualizar

--- src/main/java/br/com/cpm/api/controller/CategoriaController.java
+++ src/main/java/br/com/cpm/api/controller/CategoriaController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,6 +54,19 @@ public class CategoriaController {
     @PostMapping
     public ResponseEntity<Categoria> cadastrarCategoria(@RequestBody Categoria categoria) {
         return ResponseEntity.status(HttpStatus.CREATED).body(categoriaService.salvarCategoria(categoria));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Categoria> atualizarCategoria(@PathVariable("id") Long id, @RequestBody Categoria dadosCategoria) {
+        Optional<Categoria> categoria = categoriaService.carregarDadosCategoriaPeloId(id);
+
+        if (categoria.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        dadosCategoria.setId(id);
+
+        return ResponseEntity.ok().body(categoriaService.salvarCategoria(dadosCategoria));
     }
     
 
